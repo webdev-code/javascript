30dc6c0468a3039f88fee2fa2d10107989da4cf5
usa3a <usa3amustafa@gmail.com>
2020-07-21 22:07:05 +0500

    variables/strings

--- variables/app.js
+++ variables/app.js
@@ -3,7 +3,7 @@
 //
 // naming rules
 // naming convention camelCase
-//strings , quotation marks
+//strings ,string concatination, quotation marks , backticks and template strings
 
 const firstName = 'usama';
 
@@ -15,7 +15,17 @@ let zipCode, city, country;
 zipCode = '42200';
 city = 'mianwali';
 country = 'pakistan';
+const fullName = firstName + ' ' + secondName;
+
+// console.log('hello there your full name is : ' + firstName + ' ' + secondName);
+console.log('hello there your full name is : ' + fullName);
 
 console.log(firstName);
 console.log(secondName);
 console.log(zipCode, city, country);
+
+// url using string concatination
+const website = 'google';
+const url = 'https://' + website + '.com';
+
+console.log(url);
