1e7801ecb9cf3525f93d36f72ddc9078cd393cab
usa3a <usa3amustafa@gmail.com>
2020-07-21 11:42:54 +0500

    added comments_and_statemtnts

---
+++ comments_and_statements/app.js
@@ -0,0 +1,9 @@
+// statments are instructions or a set of instructions
+
+// single line comment
+/*
+multiline comments
+*/
+
+console.log("statements are set of instruction");
+console.log("comments are not displayed in the output");
