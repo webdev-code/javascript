9c949a723a18f49dca88410ed40f1eb7abc159e5
usa3a <usa3amustafa@gmail.com>
2020-07-21 13:23:09 +0500

    some changes in helper method

--- helper_methods/app.js
+++ helper_methods/app.js
@@ -1,3 +1,12 @@
 alert("Hello World Using alert Method");
-document.write("Hello World Using document.write method");
+document.write("Hello World Using document.write method.............");
 console.log("Hello World Using console.log method");
+
+//console.log is the best way
+
+//here is the why
+let obj = { name: "usa3a" };
+
+alert(obj);
+document.write(obj);
+console.log(obj);
