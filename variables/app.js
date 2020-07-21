2b9e6ce7b6827e2f7a3b9092e82a91f10221f0b3
usa3a <usa3amustafa@gmail.com>
2020-07-21 21:42:15 +0500

    variables added

---
+++ variables/app.js
@@ -0,0 +1,20 @@
+//variables
+// var let const    //scope //asigning value
+//
+// naming rules
+// naming convention camelCase
+
+const firstName = 'usama';
+
+//var
+var secondName;
+secondName = 'mustafa';
+//let
+let zipCode, city, country;
+zipCode = '42200';
+city = 'mianwali';
+country = 'pakistan';
+
+console.log(firstName);
+console.log(secondName);
+console.log(zipCode, city, country);
