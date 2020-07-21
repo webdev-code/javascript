ad5a83e61eba4dab71726239b665970f9e010020
usa3a <usa3amustafa@gmail.com>
2020-07-21 14:00:49 +0500

    changes in comments and statements

--- comments_and_statements/app.js
+++ comments_and_statements/app.js
@@ -5,6 +5,6 @@
 /*
 multiline comments
 */
-
+document.write("we are going to use console.log method from now onwards");
 console.log("statements are set of instruction");
 console.log("comments are not displayed in the output");
