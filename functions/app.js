981cb87ff055869877c6a7ff6c50dc9b2b7bbb9d
usa3a <usa3amustafa@gmail.com>
2020-07-22 21:16:25 +0500

    retun in functions

--- functions/app.js
+++ functions/app.js
@@ -10,11 +10,25 @@ function greetings() {
 
 //calling the function
 greetings();
-
 //some code
-
+greetings();
+//some code
 greetings();
 
-//some code
+//parameters , arguments
 
-greetings();
+const num1 = 54;
+const num2 = 45;
+
+//function with parameters
+function sum(x, y) {
+  // console.log(x + y);
+  return x + y;
+  console.log(x + y); //not run because of return
+}
+
+//passing the arguments
+console.log(sum(num1, num2));
+const ans = sum(num1, num2);
+console.log(ans);
+console.log('the sum of two numbers is :' + sum(num1, num2));
