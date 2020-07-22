eceea76b9bf2054535a7520486797893b04f413b
usa3a <usa3amustafa@gmail.com>
2020-07-22 14:24:43 +0500

    arrays done

---
+++ arrays/app.js
@@ -0,0 +1,19 @@
+//arrays , 0 index based , accessing and modifuing array elements using index
+
+const friends = ['john', 'barry', 'jonas', 'anna', 45, null, undefined];
+
+console.log(friends);
+
+let bestfriend = 'fourtyfive';
+friends[4] = bestfriend;
+
+console.log(friends);
+
+let secondBestFriend = friends[2];
+friends[4] = 'anna';
+console.log(friends);
+console.log(friends[4]);
+
+console.log(secondBestFriend);
+
+console.log(friends[3]);
