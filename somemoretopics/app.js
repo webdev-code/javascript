19a9f16418c17c32a46304b986df1390817a5275
usa3a <usa3amustafa@gmail.com>
2020-07-24 15:51:00 +0500

    scope variable lookup

--- somemoretopics/app.js
+++ somemoretopics/app.js
@@ -27,7 +27,7 @@ console.log(`the name of the second person is ${person2.name}`);
 //ternary operator
 
 // let num1 = null;
-let num1 = undefined;
+let num1 = 10;
 let num2 = 5;
 let sum = num1 + num2; //adding null and a number is equal to 0 (null) + 5 = 5
 
@@ -38,3 +38,36 @@ sum == 9
   : sum == 10
   ? console.log('sum is 10')
   : console.log('no sum');
+
+//   local Scope , global scope , function in function (scope) / variable lookup
+
+//  parent functions have no access to children functions or their variables
+//  children functions have full access to parent functions and their variables
+
+function sayHello(name) {
+  console.log(`hello ${name}`);
+}
+
+function subtraction(x, y) {
+  const z = x - y;
+  console.log(z);
+  //   console.log(result); variable result has a local scope of multiply function only
+  //function within the function
+  function multiply() {
+    const result = z * 2;
+    console.log(result); // variables outside can be accessed here
+    sayHello('johny'); // functions outside can be accessed here
+  }
+  multiply(); // this function has a scope inside the subtraction function only cannot be called outside subtraction()
+}
+
+// function subtraction() {
+//   const z = num1 - num2; //num1 and num2 have global scope and can be used/accessed any where in the code
+//   console.log(z);
+//   globalNumber = 099; //a variable without type in the function can be accessed outside of that function (global scope)
+
+// }
+
+// console.log(z); z has local scope and can be used/accessed in the function subtraction only
+subtraction(num1, num2);
+// console.log(globalNumber);
