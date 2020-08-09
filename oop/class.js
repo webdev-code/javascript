1cc83c2934c2faa3285fb799d276185ffb589791
usa3a <usa3amustafa@gmail.com>
2020-08-09 13:49:25 +0500

    classes and subclasses

--- oop/class.js
+++ oop/class.js
@@ -1,14 +1,17 @@
 // ES6 Classes
 
 class Person {
+  //constructor with parameters
   constructor(firstName, lastName, age, dob, gender) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
     this.dob = dob;
     this.gender = gender;
-    this.heOrSHe;
   }
+
+  //methods
+
   heOrSHeF = function () {
     if (this.gender === 'm') {
       this.heOrSHe = 'he';
@@ -24,3 +27,34 @@ class Person {
 const john = new Person('John', 'bradley', 34, '14 nov 1990', 'm');
 console.log(john);
 console.log(john.getSummary());
+
+//subclass Programmer
+
+class Programmer extends Person {
+  //constructor with parameters of parent(person) and own too
+  constructor(firstName, lastName, age, dob, gender, language, years) {
+    //in order to call parent constructor we use super method
+    //super function with parameters of the parent (person)
+    super(firstName, lastName, age, dob, gender);
+    this.language = language;
+    this.years = years;
+  }
+  programesIn = function () {
+    return `${this.firstName} ${this.lastName} is a ${this.language} Developer , and ${this.heOrSHe} has an experience of  ${this.years} years`;
+  };
+}
+
+//instance of Programmer
+const joe = new Programmer(
+  'Joe',
+  'Doe',
+  23,
+  '13 aug 1990',
+  'm',
+  'Javascript',
+  'three'
+);
+
+console.log(joe);
+console.log(joe.getSummary());
+console.log(joe.programesIn());
